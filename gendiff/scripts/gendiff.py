e5e43b0c91366f8508cc6fe51f432706d2f74ea9
ilyas-r <riggit5->
2023-12-22 09:20:53 +0300

    add вернул до разделения

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,15 @@
 import argparse
 import json
-from gendiff.cli import get_arg
 from gendiff.diff_finder import generate_diff
 
-def main():
-    file1, file2 = cli.get_arg()
-    result = generate_diff(file1, file2)
-    print(result)
-    
-    
 
+def main():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f FORMAT, --format FORMAT set format of output')
+    args = parser.parse_args()
+    
 if __name__ == '__main__':
     main()
+
