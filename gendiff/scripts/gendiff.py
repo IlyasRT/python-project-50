ed68cd541054b71f37cfc42f0aea3a89da44fa07
ilyas-r <riggit5->
2023-12-22 09:49:44 +0300

    add print

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,14 +2,15 @@ import argparse
 import json
 from gendiff.diff_finder import generate_diff
 
-
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f FORMAT, --format FORMAT set format of output')
     args = parser.parse_args()
-    
+    print(generate_diff(path_to_file1,path_to_file2))
+
+
 if __name__ == '__main__':
     main()
 
