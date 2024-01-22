8fdf0692cd56534599a876f6ae436c06e9aaa625
ilyas-r <riggit5->
2024-01-22 12:24:58 +0300

    correction test_diff_finder.py

--- tests/test_diff_finder.py
+++ tests/test_diff_finder.py
@@ -1,6 +1,23 @@
 import json
-from gendiff.diff_finder import diff_finder
+from gendiff.diff_finder import generate_diff
 
 def test_generate_diff():
-    assert  generate_diff(path_to_file1, path_to_file2) == {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
+    assert  generate_diff(file1.json, file2.json) == {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
+    
+    
+'''
+def test_generate_diff():
+    assert  generate_diff(./tests/fixtures/file1.json, ./tests/fixtures/file2.json) == {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
+
+def test_generate_diff():
+    assert  generate_diff(/fixtures/file1.json, /fixtures/file2.json) == {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
+    
+def test_generate_diff():
+    assert  generate_diff(./fixtures/file1.json, ./fixtures/file2.json) == {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
+
+def test_generate_diff():
+    assert  generate_diff(fixtures/file1.json, fixtures/file2.json) == {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
+
+'''
+
 
