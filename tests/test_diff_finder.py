0f2cc9e2ce459f04a2de382550df83a5428f6b18
ilyas-r <riggit5->
2024-01-24 10:05:54 +0300

    add correcction test_diff_finder.py

--- tests/test_diff_finder.py
+++ tests/test_diff_finder.py
@@ -1,11 +1,11 @@
 import json
 from gendiff.diff_finder import generate_diff
-import test.fixtures.expected_comparison_result
+#import test.fixtures.expected_comparison_result
 
 
 
 def test_generate_diff():
 
-    assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == result = {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
+    assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
 
     # assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == expected_comparison_result.result
