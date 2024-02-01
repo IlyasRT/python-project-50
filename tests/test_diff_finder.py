0d991fdbbd0ae542d75ed60c3b1e80cbe7c3ab43
ilyas-r <riggit5->
2024-02-01 11:23:21 +0300

    add expected_results.txt (does not working)

--- tests/test_diff_finder.py
+++ tests/test_diff_finder.py
@@ -1,8 +1,19 @@
 import json
 from gendiff.diff_finder import generate_diff
-from tests.fixtures.expected_results import result
-
+#from tests.fixtures.expected_results import result
 
 
 def test_generate_diff():
-    assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == result
+    with open("./tests/fixtures/expected_results.txt", encoding='utf8') as file:
+        result = file.read()
+        print(result)
+        result_json = json.loads(result)
+        print(result_json)
+
+    assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == result_json
+    
+    
+    
+# для тестов оставил 
+# result = {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
+        
