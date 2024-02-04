04c343b646c7665c004c18ff5f3ce4d5948c1809
ilyas-r <riggit5->
2024-02-04 11:02:05 +0300

    add test files

--- gendiff/diff_finder.py
+++ gendiff/diff_finder.py
@@ -7,11 +7,11 @@ def generate_diff(path_to_file1, path_to_file2):
     content_file_3 = {}
     for k, v in sorted(content_file_1.items()):
         if k not in content_file_2:
-            k =' - ' + str(k)
+            k =" - " + str(k)
             content_file_3[k] = v
         if k in content_file_2 and content_file_1[k] != content_file_2[k]:
-            content_file_3[' - ' + str(k)] = v
-            content_file_3[' + ' + str(k)] = content_file_2[k]
+            content_file_3[" - " + str(k)] = v
+            content_file_3[" + " + str(k)] = content_file_2[k]
         if k in content_file_2 and content_file_1[k] == content_file_2[k]:
             content_file_3[k] = v
         if k in content_file_2 and content_file_1[k] == content_file_2[k]:
@@ -19,5 +19,5 @@ def generate_diff(path_to_file1, path_to_file2):
     key_not_in_content_file_1 = list(set(content_file_2).difference(set(content_file_1)))
     for i in key_not_in_content_file_1:
         if i in content_file_2:
-            content_file_3[' + '+str(i)] = content_file_2[i]
+            content_file_3[" + "+str(i)] = content_file_2[i]
     return content_file_3
--- tests/test_diff_finder.py
+++ tests/test_diff_finder.py
@@ -1,19 +1,25 @@
 import json
+import pytest
 from gendiff.diff_finder import generate_diff
 #from tests.fixtures.expected_results import result
 
 
+'''
+Вариант 0 - использовал для теста, чтобы проверить все остальное кроме импорта expected_results.txt
+result = {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
 def test_generate_diff():
-    with open("./tests/fixtures/expected_results.txt", encoding='utf8') as file:
-        result = file.read()
-        print(result)
-        result_json = json.loads(result)
-        print(result_json)
+    assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == result
+'''
 
-    assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == result_json
-    
-    
-    
-# для тестов оставил 
-# result = {' - follow': False, 'host': 'hexlet.io', ' - proxy': '123.234.53.22', ' - timeout': 50, ' + timeout': 20, ' + verbose': True}
-        
+'''
+Вариант 1 - для полного сравнения мешают двойные кавычки "{ перед знаком
+def test_generate_diff():
+    with open("./tests/fixtures/expected_results.txt", 'r', encoding='utf8') as file:
+        result = file.read().strip('\n')
+        assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == result
+'''
+
+
+def test_generate_diff():
+    result = json.load(open("./tests/fixtures/expected_results.txt")) #  загружает json и выгружает в виде строки, но тоже не проходит тесты
+    assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == result
