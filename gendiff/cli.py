54a2c68c7bee3e17d2e77281135c236676ee34f1
ilyas-r <riggit5->
2024-02-05 08:42:17 +0300

    вынес часть кода из скрипат в gendiff/cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -2,10 +2,9 @@ import argparse
 import json
 
 
-def get_arg():
+def get_parser_args():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f FORMAT, --format FORMAT set format of output')
-    args = parser.parse_args()
-    # return a_1, a_2, a_3
+    parser.add_argument('-f', '--format', help='set format of output')
+    return parser.parse_args()
