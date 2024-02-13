45079509263709ccc81a1325b97030f2f33c5d8f
ilyas-r <riggit5->
2024-02-13 09:07:00 +0300

    импорт pyyaml и коррекция тестовexit

--- tests/test_diff_finder.py
+++ tests/test_diff_finder.py
@@ -1,6 +1,7 @@
 import json
 import yaml
 import pytest
+#import PyYAML
 from gendiff.diff_finder import generate_diff
 # from tests.fixtures.expected_results import result
 
@@ -8,6 +9,6 @@ from gendiff.diff_finder import generate_diff
 
 def test_generate_diff():
     with open("./tests/fixtures/expected_results.txt", 'r', encoding='utf8') as file:
-        # result = file.read().strip('\n')
-        result = file.read()
-        assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', stylish) == result
+        result = file.read().strip('\n')
+        #result = file.read()
+        assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', 'stylish') == result
