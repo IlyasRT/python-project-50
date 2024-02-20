b32305f4d664e836bfcc1f5c1dfed95e193eddd6
ilyas-r <riggit5->
2024-02-20 11:34:19 +0300

    correct cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -7,5 +7,9 @@ def get_parser_args():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output', default='stylish', type=str)
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output',
+        default='stylish', type=str
+    )
     return parser.parse_args()
