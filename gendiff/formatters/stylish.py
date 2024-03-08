feadf8f7fb71871b1d0e8d8af00c01acf27771bb
IlyasRT <104883100+IlyasRT@users.noreply.github.com>
2024-03-08 17:16:17 +0300

    Update stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -12,7 +12,8 @@ def to_str(value, spaces_count=2, depth=1):
         return str(value).lower()
     if isinstance(value, dict):
         # indent = SEPARATOR * (spaces_count + DEFAULT_INDENT)
-        indent = SEPARATOR * (depth * DEFAULT_INDENT - 2)
+        # indent = SEPARATOR * (depth * DEFAULT_INDENT - 2)
+        indent = SEPARATOR * (depth * DEFAULT_INDENT)
         lines = []
         for key, inner_value in value.items():
             formatted_value = to_str(
