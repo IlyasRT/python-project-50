4712725ed4256ba894de16ed4346417115f2703d
IlyasRT <104883100+IlyasRT@users.noreply.github.com>
2024-03-08 20:36:44 +0300

    Update stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -24,7 +24,7 @@ def to_str(value, spaces_count=2, depth=1):
             )
             lines.append(f"{indent}{NONE}{key}: {formatted_value}")
         formatted_string = '\n'.join(lines)
-        end_indent = SEPARATOR * (spaces_count + 2)
+        end_indent = SEPARATOR * (4 * depth - 6)
         # end_indent = SEPARATOR * (spaces_count + 2)
         return f"{{\n{formatted_string}\n{end_indent}}}"
     return f"{value}"
