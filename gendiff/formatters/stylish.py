780bab41326a68d4bd6b13f0badbe3bb5f6d5911
IlyasRT <104883100+IlyasRT@users.noreply.github.com>
2024-03-08 19:37:51 +0300

    Update stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -5,7 +5,7 @@ NONE = '  '
 DEFAULT_INDENT = 4
 
 
-def to_str(value, spaces_count=2, depth=1):
+def to_str(value, spaces_count=2, depth):
     if value is None:
         return "null"
     if isinstance(value, bool):
